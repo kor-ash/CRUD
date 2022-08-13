9fa2f79d69bf6a2ddaa5e924f52f9b20b7729972
kor-ash <109964592+kor-ash@users.noreply.github.com>
2022-08-13 23:18:08 +0900

    Day 4 fin

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import styles from './Content.module.css'
+import { faE, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Content = ({ boardName }) => {
     return (
-        <div className={styles.Content}>
-            {boardName}
+        <div className={styles.ContentContainer}>
+            <div className={styles.ContentHeader}>
+                <div className={styles.ContentTitle}>{boardName}</div>
+                <div className={styles.ContentSearch}>
+                    <input placeholder="검색어를 입력하세요" />
+                    <FontAwesomeIcon className={styles.ContentButtonSearch} icon={faSearch} />
+                    <FontAwesomeIcon className={styles.ContentButtonEdit} icon={faEdit} />
+                </div>
+            </div>
+            <div className={styles.ContentList}>
+
+            </div>
         </div >
     );
 };
